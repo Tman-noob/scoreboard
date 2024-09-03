499e8910029ba5fca8ada5ef7255978124a42106
Tman <91443186+Tman-noob@users.noreply.github.com>
2024-09-03 16:33:15 -0500

    Made shell of app

---
+++ index.js
@@ -0,0 +1,34 @@
+let leftEl = document.getElementById("left-btn");
+let rightEl = document.getElementById("right-btn");
+let leftRmvEl = document.getElementById("left-btn-rmv");
+let rightRmvEl = document.getElementById("right-btn-rmv");
+let resetEl = document.getElementById("reset-btn");
+let leftCount = 0;
+let rightCount = 0;
+
+function left() {
+    leftCount += 1;
+    leftEl.textContent = leftCount;
+}
+
+function leftRmv() {
+    leftCount -= 1;
+    leftEl.textContent = leftCount;
+}
+
+function right() {
+    rightCount += 1;
+    rightEl.textContent = rightCount;
+}
+
+function rightRmv() {
+    rightCount -= 1;
+    rightEl.textContent = rightCount;
+}
+
+function reset() {
+    leftCount *= 0;
+    rightCount *= 0;
+    rightEl.textContent = 0;
+    leftEl.textContent = 0;
+}
