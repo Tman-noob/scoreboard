bc2b48dd6a9a74cf8417da73b9784159215bd648
Tman-noob <91443186+Tman-noob@users.noreply.github.com>
2024-09-16 15:59:28 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -8,11 +8,18 @@ let rightCount = 0;
 
 function left() {
     leftCount += 1;
-    leftEl.textContent = leftCount;
+    if (leftCount < 10) {
+        leftEl.textContent = "0" + leftCount;
+    } else {
+        leftEl.textContent = leftCount;
+    }
 }
 
 function leftRmv() {
-    if (leftCount > 0) {
+    if (leftCount > 0 && leftCount <= 10) {
+        leftCount -= 1;
+        leftEl.textContent = "0" + leftCount;
+    } else if (leftCount > 10) {
         leftCount -= 1;
         leftEl.textContent = leftCount;
     }
@@ -20,11 +27,18 @@ function leftRmv() {
 
 function right() {
     rightCount += 1;
-    rightEl.textContent = rightCount;
+    if (rightCount < 10) {
+        rightEl.textContent = "0" + rightCount;
+    } else {
+        rightEl.textContent = rightCount;
+    }
 }
 
 function rightRmv() {
-    if (rightCount > 0) {
+    if (rightCount > 0 && rightCount <= 10) {
+        rightCount -= 1;
+        rightEl.textContent = "0" + rightCount;
+    } else if (rightCount > 10) {
         rightCount -= 1;
         rightEl.textContent = rightCount;
     }
@@ -33,6 +47,6 @@ function rightRmv() {
 function reset() {
     leftCount *= 0;
     rightCount *= 0;
-    rightEl.textContent = 0;
-    leftEl.textContent = 0;
+    rightEl.textContent = "00";
+    leftEl.textContent = "00";
 }
